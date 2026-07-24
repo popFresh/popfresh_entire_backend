92b12bb61593461f478f5a2df39bb734fcd603dd
officialmohitagarwal <officialmohitagarwal@gmail.com>
2026-07-24 23:02:37 +0530

    testing webhook shiprocket

--- src/controllers/shiprocketWebhook.controller.js
+++ src/controllers/shiprocketWebhook.controller.js
@@ -1,43 +1,55 @@
 import { processShiprocketWebhook } from "../services/shiprocketWebhook.service.js";
 
-export const handleShiprocketWebhook = async (
-  req,
-  res,
-  next
-) => {
 
-  try {
+export const handleShiprocketWebhook = async (req, res) => {
+  console.log("🔥 SHIPROCKET HIT");
+  console.log("Headers:", req.headers);
+  console.log("Body:", req.body);
 
-    // const apiKey = req.headers["x-api-key"];
-
-    // if (
-    //   apiKey !== process.env.SHIPROCKET_WEBHOOK_SECRET
-    // ) {
-
-    //   return res.status(401).json({
-
-    //     success: false,
-
-    //     message: "Unauthorized webhook.",
-
-    //   });
-
-    // }
-
-    await processShiprocketWebhook(req.body);
-
-    return res.status(200).json({
-
-      success: true,
-
-      message: "Webhook received successfully.",
-
-    });
-
-  } catch (error) {
-
-    next(error);
-
-  }
-
+  return res.status(200).json({
+    success: true,
+    message: "Webhook reached",
+  });
 };
+
+// export const handleShiprocketWebhook = async (
+//   req,
+//   res,
+//   next
+// ) => {
+
+//   try {
+
+//     // const apiKey = req.headers["x-api-key"];
+
+//     // if (
+//     //   apiKey !== process.env.SHIPROCKET_WEBHOOK_SECRET
+//     // ) {
+
+//     //   return res.status(401).json({
+
+//     //     success: false,
+
+//     //     message: "Unauthorized webhook.",
+
+//     //   });
+
+//     // }
+
+//     await processShiprocketWebhook(req.body);
+
+//     return res.status(200).json({
+
+//       success: true,
+
+//       message: "Webhook received successfully.",
+
+//     });
+
+//   } catch (error) {
+
+//     next(error);
+
+//   }
+
+// };
